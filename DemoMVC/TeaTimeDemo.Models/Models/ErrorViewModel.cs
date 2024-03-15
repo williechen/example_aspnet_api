492b6824cb116ab6aabbf8e6886d3477e2c8df55
Chen Willie Guan Yu <bcskcnuj@hotmail.com>
2024-03-15 09:34:20 +0800

    modify dir

--- DemoMVC/TeaTimeDemo.Models/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace TeaTimeDemo.Models.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
